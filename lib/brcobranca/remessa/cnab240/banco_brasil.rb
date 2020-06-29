b1330e59c1f162ad3a21bd62622ee8654be13b0f
joaowgs <joao.weege@senior.com.br>
2020-06-29 11:17:19 -0300

    Ajuste do campo aceite na geração da remessa

--- lib/brcobranca/remessa/cnab240/banco_brasil.rb
+++ lib/brcobranca/remessa/cnab240/banco_brasil.rb
@@ -21,8 +21,7 @@ module Brcobranca
         def initialize(campos = {})
           campos = { emissao_boleto: '0',
             distribuicao_boleto: '0',
-            codigo_baixa: '00',
-            aceite:'N',}.merge!(campos)
+            codigo_baixa: '00',}.merge!(campos)
           super(campos)
         end
 
