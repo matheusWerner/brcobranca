aba4a01c583291de1f4fc99161670fdbf3d9eb54
joaowgs <joao.weege@senior.com.br>
2020-11-16 10:50:18 -0300

    Testes

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -263,7 +263,7 @@ module Brcobranca
             move_more(doc, 0, -2)
           else
             move_more(doc, -15.8, -0.9)
-            doc.show boleto.instrucao1.replace('R$Â', 'R$')
+            doc.show boleto.instrucao1
 
             move_more(doc, 0, -0.4)
             doc.show boleto.instrucao2
